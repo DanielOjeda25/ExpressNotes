7544731614cacf1a4c8aad3d99cfee031d8f34da
DanielOjeda25 <ojedadanielalejandro333@gmail.com>
2022-09-09 13:08:37 -0300

    links routes

--- src/routes/links.js
+++ src/routes/links.js
@@ -2,4 +2,12 @@ const express = require('express')
 const router = express.Router()
 // conesto hacemos la conexion a la base de datos
 const pool = require('../database')
+
+router.get('/add', (req, res) => {
+  res.render('links/add')
+})
+router.post('/add', (req, res) => {
+  res.send('recibido')
+})
+
 module.exports = router
