7a85f4fd4abd37c48de26aacce290de5e04c37c0
DanielOjeda25 <ojedadanielalejandro333@gmail.com>
2022-09-10 14:16:26 -0300

    Autenticacion

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,4 +1,21 @@
 const express = require('express')
+const passport = require('passport')
 const router = express.Router()
+// esta ruta es para renderizar el formulario
+router.get('/signup', (req, res) => {
+  res.render('auth/signup')
+})
+// esta ruta es para enviar los datos del form
+router.post(
+  '/signup',
+  passport.authenticate('local.signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup',
+    failureFlash: true
+  })
+)
 
+router.get('/profile', (req, res) => {
+  res.send('LLEGASTE')
+})
 module.exports = router
